722bc9bdf894e727752682b94c336936bbec5450
unclehking <513935463@qq.com>
2017-05-24 10:59:21 +0800

    new

--- jquery.h5upload.js
+++ jquery.h5upload.js
@@ -24,20 +24,21 @@
             		var _t = this;
             		for(var i=0,len=this.files.length;i<len;i++){
             			var file = this.files[i];
+//          			if($("#"+file.lastModified).length > 0){
+//          				alert("请勿选择重复文件！");
+//          				return false;
+//          			}
 	            		var reader = new FileReader();
 						reader.readAsDataURL(file);
 						reader.onload = function(e){
-							var fileItem = $('<div class="fileItem"><img src="'+this.result+'" /><div class="cancle" title="删除">X</div></div>');
+							var fileItem = $('<div class="fileItem" id="'+file.lastModified+'"><img src="'+this.result+'" /><div class="cancle" title="删除">X</div></div>');
 							_this.append(fileItem);
 							fileItem.data("file",file);
-							
 							fileItem.find(".cancle").click(function(){
-								//$(this).parent().remove();
-								console.log($(this).parent().data("file"));
-								var fileData = {
-								};
-								options.onCancel($(this).parent().data("file"));
+								var fileData = $(this).parent().data("file");
 								$(this).parent().remove();
+								var allData = getAllData();
+								options.onCancel(fileData,allData);
 							});
 							sendFile(file,fileItem);
 						};
@@ -51,7 +52,16 @@
             		_this.find("#fileInput").trigger('click');
             	}
             })
-            this.append(fileInput).append(selectBtn);
+            var uploading = $('<div class="uploading"><div><i class="fa fa-spinner fa-pulse fa-2x"></i><p>上传中...</p></div></div>');
+            this.append(fileInput).append(selectBtn).append(uploading);
+            
+            function getAllData(){
+            	var allData = [];
+            	$(".fileItem").each(function(i,t){
+            		allData.push($(t).data("file"));
+            	});
+            	return allData;
+            }
             
             function sendFile(file,item){
             	var fd = new FormData();
@@ -64,14 +74,17 @@
 		            contentType: false,
 		            data: fd,
 		            beforeSend:function(data){
-		            	
+		            	$(_this).find(".uploading").show();
 		            },
 		            success: function (data) {
-		                options.success(data,item)
+		            	$(item).data("file",data);
+		            	var allData = getAllData();
+		                options.success(data,allData,item)
+		                $(_this).find(".uploading").hide();
 		            }
 		        });
             }
-            
+            return this;
         }  
     });  
   
